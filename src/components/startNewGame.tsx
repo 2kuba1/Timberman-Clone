899f90470566c42d0857e34819ce34ae28c2816c
2kuba1 <jakub.wojtyna06@gmail.com>
2023-04-13 00:01:03 +0200

    Add sound effect on click submit button

--- src/components/startNewGame.tsx
+++ src/components/startNewGame.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
+import { useRef } from "react";
 
 const StartNewGame = () => {
+  const musicPlayers = useRef<HTMLAudioElement | undefined>(
+    typeof Audio !== "undefined" ? new Audio("/menu.mp3") : undefined
+  );
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="flex h-screen w-screen items-center justify-center backdrop-blur-sm">
       <div className="flex h-4/5 w-3/4 flex-col items-center rounded-xl bg-red-900 p-1">
@@ -11,7 +20,10 @@ const StartNewGame = () => {
           width="250"
           className="relative top-5"
         />
-        <form className="relative top-16 flex h-1/2 w-full flex-col items-center gap-5">
+        <form
+          className="relative top-16 flex h-1/2 w-full flex-col items-center gap-5"
+          onSubmit={handleSubmit}
+        >
           <label className="text-3xl font-bold text-menu-yellow">
             Username
           </label>
@@ -22,7 +34,8 @@ const StartNewGame = () => {
           <input
             type="submit"
             value="Start"
-            className="h-10 w-2/3 rounded-xl bg-menu-yellow font-bold text-red-900"
+            className="h-10 w-2/3 cursor-pointer rounded-xl bg-menu-yellow font-bold text-red-900 duration-100 active:scale-110 active:bg-menu-copper"
+            onClick={() => musicPlayers.current?.play()}
           />
         </form>
         <div className="relative flex w-full flex-col items-center gap-6">
