45fd09ad520b7827948de2b2d33f8438e2cbac44
2kuba1 <jakub.wojtyna06@gmail.com>
2023-04-15 14:39:27 +0200

    Add man and animation

--- src/components/playerMovement.tsx
+++ src/components/playerMovement.tsx
@@ -4,45 +4,59 @@ import useDetectKeyPress from "~/hooks/useDetectKeyPress";
 
 const PlayerMovement = () => {
   const [playerPosition, setPlayerPosition] = useState(0); // 0 = standing, 1 =  left, 2  = right
-  const [lastPostion, setLastPostion] = useState("justify-start"); // 0 = standing, 1 =  left, 2  = right
+  const [lastPostion, setLastPostion] = useState("justify-start");
+
+  const [animationStage, setAnimationStage] = useState(0);
 
   useDetectKeyPress("ArrowLeft", async () => {
     setPlayerPosition(1);
     setLastPostion("justify-start");
     console.log("left");
-    await cancelKey();
+    setTimeout(() => {
+      setAnimationStage(1);
+      setTimeout(() => {
+        setAnimationStage(2);
+        setTimeout(() => {
+          setAnimationStage(0);
+        }, 100);
+      }, 100);
+    }, 100);
   });
   useDetectKeyPress("ArrowRight", async () => {
     setPlayerPosition(2);
     setLastPostion("justify-end");
     console.log("right");
-    await cancelKey();
-  });
-
-  const cancelKey = async () => {
-    const detectKeyUp = (e: KeyboardEvent) => {
-      if (e.key === "ArrowLeft" || e.key === "ArrowRight") return;
-    };
-    window.addEventListener("keyup", detectKeyUp);
-    return new Promise((resolve) =>
+    setTimeout(() => {
+      setAnimationStage(1);
       setTimeout(() => {
-        setPlayerPosition(0);
-        removeEventListener("keyup", detectKeyUp);
-        resolve(true);
-      }, 400)
-    );
-  };
+        setAnimationStage(2);
+        setTimeout(() => {
+          setAnimationStage(0);
+        }, 100);
+      }, 100);
+    }, 100);
+  });
 
   return (
     <div
-      className={`$ relative bottom-4 flex h-full w-full items-end ${
+      className={`relative bottom-4 flex h-full w-full items-end ${
         playerPosition === 0
           ? lastPostion
           : playerPosition === 1
           ? "justify-start"
           : "justify-end"
       }`}
-    ></div>
+    >
+      {animationStage === 0 && (
+        <Image src="/man1.png" alt="man" height="128" width="128" />
+      )}
+      {animationStage === 1 && (
+        <Image src="/man2.png" alt="man" height="128" width="128" />
+      )}
+      {animationStage === 2 && (
+        <Image src="/man3.png" alt="man" height="128" width="128" />
+      )}
+    </div>
   );
 };
 
