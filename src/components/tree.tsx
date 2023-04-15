a67592aa1fb0ff94bf02a1ab9c5f5100d4129af7
pabloscyzoryk <pawel.cyrzyk@gmail.com>
2023-04-15 20:37:57 +0200

    started working on killing timberman

--- src/components/tree.tsx
+++ src/components/tree.tsx
@@ -12,7 +12,6 @@ const Tree: FC<Props> = ({treeBlocks}) => {
   return (
     <div className="absolute left-1/2 top-[45%] flex h-screen w-screen -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-end">
       {treeBlocks.map((block, index) => (
-        <div className="flex">
           <Image
             src={block as string}
             key={index}
@@ -21,7 +20,6 @@ const Tree: FC<Props> = ({treeBlocks}) => {
             height="200"
             className="m-0"
           />
-        </div>
       ))}
       <Image
         className="relative left-[2px] m-0"
