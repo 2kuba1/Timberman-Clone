f9c45396dd7db7d366bbbc8eacfa8c87174d6f25
2kuba1 <jakub.wojtyna06@gmail.com>
2023-04-19 20:29:38 +0200

    Add overflow

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -20,7 +20,8 @@ const Leaderboard = () => {
           rel="stylesheet"
         />
       </Head>
-      <div className='mt-5 flex h-screen w-screen items-center gap-5 overflow-x-scroll bg-[url("/background.png")] bg-cover text-xl font-bold lg:bg-contain lg:text-2xl '>
+      <div className='flex h-screen w-screen items-center gap-5 overflow-y-auto bg-[url("/background.png")] bg-cover text-xl font-bold lg:bg-contain lg:text-2xl '>
+        <div></div>
         <div className="relative top-5 flex h-full w-full flex-col items-center gap-5">
           {scores && scores.length > 0 ? (
             scores?.map((score, index) => (
@@ -30,7 +31,7 @@ const Leaderboard = () => {
               >
                 <div className="relative left-5 flex items-center gap-3 text-sm">
                   <div className="flex items-center gap-1 text-xl">
-                    <span>{index + 1}#</span>{" "}
+                    <span>{index + 1}#</span>
                     {index + 1 <= 3 && (
                       <div
                         className={`text-menu-${
@@ -43,10 +44,10 @@ const Leaderboard = () => {
                       >
                         <TfiCup />
                       </div>
-                    )}{" "}
+                    )}
                   </div>
                   <div>{score.username}</div>
-                </div>{" "}
+                </div>
                 <div className="relative right-10 flex items-center gap-2">
                   {score.score} <GiLog />
                 </div>
