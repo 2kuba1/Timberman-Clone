55806e25acc2426c5d5aa1bb298a8b72fa0c6c6a
2kuba1 <jakub.wojtyna06@gmail.com>
2023-04-19 20:01:01 +0200

    Fix eslint errors

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -8,15 +8,21 @@ const Leaderboard = () => {
   return (
     <div className='mt-5 flex h-screen w-screen items-center gap-5 overflow-x-scroll bg-[url("/background.png")] bg-cover text-xl font-bold lg:bg-contain lg:text-2xl '>
       <div className="relative top-5 flex h-full w-full flex-col items-center gap-5">
-        {(scores && scores.length > 0) ? scores?.map((score, index) => (
-
-        <div className="flex h-28 w-4/5 flex-wrap items-center justify-around rounded-md bg-white ">
-          <div>{index + 1}# {score.username}</div>{" "}
-          <div className="flex items-center gap-2">
-            {score.score} <GiLog />
-          </div>
-        </div>
-        )) : (
+        {scores && scores.length > 0 ? (
+          scores?.map((score, index) => (
+            <div
+              key={score.id}
+              className="flex h-28 w-4/5 flex-wrap items-center justify-around rounded-md bg-white "
+            >
+              <div>
+                {index + 1}# {score.username}
+              </div>{" "}
+              <div className="flex items-center gap-2">
+                {score.score} <GiLog />
+              </div>
+            </div>
+          ))
+        ) : (
           <div>no scores yet</div>
         )}
       </div>
