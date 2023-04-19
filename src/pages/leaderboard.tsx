6f7cdbc6541f73797388855dab78120b23241038
pabloscyzoryk <pawel.cyrzyk@gmail.com>
2023-04-20 00:41:50 +0200

    added home btn and finished responsiveness

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -5,6 +5,8 @@ import Head from "next/head";
 import { TfiCup } from "react-icons/tfi";
 import { ClipLoader } from "react-spinners";
 import type { Player } from "~/types/player";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Leaderboard = () => {
   const { data: players} = api.scoreboard.getAllScores.useQuery(undefined, {
@@ -24,8 +26,11 @@ const Leaderboard = () => {
         />
       </Head>
       <div className='flex h-screen w-screen items-center gap-5 overflow-y-auto bg-[url("/background.png")] bg-cover p-5 text-xl font-bold lg:bg-contain lg:text-2xl'>
-        <div className="flex min-h-[90%] max-h-full w-full flex-wrap items-center justify-center gap-5 overflow-y-scroll">
-          <div className='text-sm px-10 flex lg:w-3/5 w-[100%] text-white justify-around'>
+        <Link href='/'>
+      <Image className='fixed top-5 left-5' alt='back to homepage button' src='/home.png' width='100' height='100'/>
+        </Link>
+        <div className="flex min-h-[90%] max-h-[90%] w-full flex-wrap items-center justify-center gap-5 overflow-y-scroll">
+          <div className='h-10 mb-3 relative top-10 text-sm px-10 flex lg:w-3/5 w-[100%] text-white justify-around'>
           <div>nick</div>
           <div>|</div>
           <div>best score</div>
@@ -36,7 +41,7 @@ const Leaderboard = () => {
                 key={index}
                 className="flex h-16 lg:w-3/5 w-[100%] flex-wrap items-center justify-between rounded-md bg-white"
               >
-                <div className=" relative left-5 flex items-center gap-3 text-sm">
+                <div className="relative left-2 lg:left-5 flex items-center gap-3 text-sm">
                   <div className="flex items-center gap-1 text-xl">
                     <span>{index + 1}#</span>
                     {index + 1 <= 3 && (
@@ -53,9 +58,9 @@ const Leaderboard = () => {
                       </div>
                     )}
                   </div>
-                  <div>{player.username}</div>
+                  <div className='text-sm'>{player.username}</div>
                 </div>
-                <div className="relative right-10 flex items-center gap-2">
+                <div className="relative lg:right-10 right-2 flex items-center gap-2 lg:text-lg text-sm">
                   {player.score} <GiLog />
                 </div>
               </div>
